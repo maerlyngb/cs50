3d175f7843412f292c30a3cad5ade2fd8319084e
Maerlyn Broadbent <maerlyn.broadbent@ge.com>
2016-06-06 10:57:04 +1000

    added file comments

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,3 +1,11 @@
+/*
+    Author: Maerlyn Broadbent
+    Created: June 5 2016
+    Licence: GPLv3
+
+    Purpose: CS50 Introduction to Computer Science, Problem Set 1
+*/
+
 #include <stdio.h>
 
 int main(void){
